5de44c51b7ac2bda9fb2717024a856b2543f7836
Julia Jaworska <ulia012008@gmail.com>
2025-05-03 17:14:09 +0200

    header close=>click

--- src/js/mobileMenu.js
+++ src/js/mobileMenu.js
@@ -1,15 +1,47 @@
 export function mobileMenu() {
-  const menu = document.querySelector('[data-menu]');
-  const burgerBtn = document.querySelector('[data-menu-open]');
-  const closeBtn = document.querySelector('[data-menu-close]');
+  const openMenuBtns = document.querySelectorAll('[data-menu-open]');
+  const closeMenuBtn = document.querySelector('[data-menu-close]');
+  const mobileMenu = document.querySelector('[data-menu]');
+  const navLinks = document.querySelectorAll('[data-menu] a');
+  const body = document.body;
 
-  burgerBtn.addEventListener('click', () => {
-    menu.classList.add('is-open');
-    document.body.classList.add('menu-open');
+  const openMenu = () => {
+    mobileMenu.setAttribute('data-menu-visible', 'true');
+    body.setAttribute('data-lock-scroll', 'true');
+  };
+
+  const closeMenu = () => {
+    mobileMenu.removeAttribute('data-menu-visible');
+    body.removeAttribute('data-lock-scroll');
+  };
+
+  openMenuBtns.forEach(btn => {
+    btn.addEventListener('click', openMenu);
   });
 
-  closeBtn.addEventListener('click', () => {
-    menu.classList.remove('is-open');
-    document.body.classList.remove('menu-open');
+  if (closeMenuBtn) {
+    closeMenuBtn.addEventListener('click', closeMenu);
+  }
+
+  navLinks.forEach(link => {
+    link.addEventListener('click', e => {
+      e.preventDefault();
+      const targetId = link.getAttribute('href').slice(1);
+      const target = document.getElementById(targetId);
+
+      if (target) {
+        const headerOffset = window.innerWidth >= 1200 ? 100 : 60;
+        const elementPosition = target.getBoundingClientRect().top;
+        const offsetPosition =
+          elementPosition + window.pageYOffset - headerOffset;
+
+        window.scrollTo({
+          top: offsetPosition,
+          behavior: 'smooth',
+        });
+
+        closeMenu();
+      }
+    });
   });
 }
--- src/main.js
+++ src/main.js
@@ -1,24 +1,5 @@
 import { mobileMenu } from './js/mobileMenu.js';
+import { handleCookieBanner } from './js/coocies.js';
 
 mobileMenu();
-document.addEventListener('DOMContentLoaded', function () {
-  const isLoggedIn = true; // заміни на перевірку, чи залогінений користувач
-  const banner = document.querySelector('[data-banner]');
-  const acceptBtn = document.querySelector('[data-accept]');
-  const declineBtn = document.querySelector('[data-decline]');
-
-  const hasConsent = localStorage.getItem('cookieConsent');
-
-  if (isLoggedIn && !hasConsent) {
-    banner.style.display = 'block';
-  }
-
-  acceptBtn.addEventListener('click', function () {
-    localStorage.setItem('cookieConsent', 'accepted');
-    banner.style.display = 'none';
-  });
-
-  declineBtn.addEventListener('click', function () {
-    banner.style.display = 'none';
-  });
-});
+handleCookieBanner();
