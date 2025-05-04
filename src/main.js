3e4d73a1b402882db8c7abb0b263fce509b8cc96
Hanna Assaf <145717934+HannaAssaf@users.noreply.github.com>
2025-05-04 20:51:49 +0100

    mobile

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { mobileMenu } from './js/mobileMenu.js';
 import { handleCookieBanner } from './js/coocies.js';
 
+import './scss/styles.scss';
+import './js/characters.js';
+
 mobileMenu();
 handleCookieBanner();
